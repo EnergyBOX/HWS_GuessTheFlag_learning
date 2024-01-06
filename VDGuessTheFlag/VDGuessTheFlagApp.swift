075b582d0a8307b32d044ff881ab73fb377b0b6d
User <user@mail.net>
2024-01-06 23:55:12 +0200

    Initial Commit

---
+++ VDGuessTheFlag/VDGuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  VDGuessTheFlagApp.swift
+//  VDGuessTheFlag
+//
+//  Created by Server Admin on 06.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct VDGuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
