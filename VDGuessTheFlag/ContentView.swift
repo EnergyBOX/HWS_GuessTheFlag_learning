bc3e66a977b294db57477cf315b4095d1f6face0
User <user@mail.net>
2024-01-10 23:01:47 +0200

    my result before lesson "Updating our design -- Guess the Flag SwiftUI Tutorial 9/9"

--- VDGuessTheFlag/ContentView.swift
+++ VDGuessTheFlag/ContentView.swift
@@ -10,37 +10,67 @@ struct ContentView: View {
     
     var body: some View {
         ZStack {
-            LinearGradient(colors: [.blue, .black], startPoint: .top, endPoint: .bottom)
-                .ignoresSafeArea()
+            RadialGradient(stops: [
+                .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
+                .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3)
+            ], center: .top, startRadius: 200, endRadius: 700)
+            .ignoresSafeArea()
             
-            VStack(spacing: 30) {
-                VStack {
-                    Text("Tap the flag of")
-                        .foregroundStyle(.white)
-                        .font(.subheadline.weight(.heavy))
-                    
-                    Text(countries[correctAnswer])
-                        .foregroundStyle(.white)
-                        .font(.largeTitle.weight(.semibold))
-                }
+            VStack {
+                Spacer()
+                Spacer()
                 
-                ForEach(0..<3) { number in
-                    Button {
-                        flagTapped(num: number)
+                Text("Gues the Flag")
+                    .foregroundStyle(.white)
+                    .font(.largeTitle.bold())
+                
+         
+                
+                VStack(spacing: 10) {
+                    VStack {
+                        Text("Tap the flag of")
+                            .foregroundStyle(.secondary)
+                            .font(.subheadline.weight(.heavy))
                         
-                    } label: {
-                        Image(countries[number])
-                            .clipShape(Capsule())
-                            .shadow(radius: 5)
-                        
+                        Text(countries[correctAnswer])
+                            .font(.largeTitle.weight(.semibold))
+                    }
+                    
+                    ForEach(0..<3) { number in
+                        Button {
+                            flagTapped(num: number)
+                            
+                        } label: {
+                            Image(countries[number])
+                                .clipShape(Capsule())
+                                .shadow(radius: 5)
+                            
+                        }
                     }
                 }
+                .frame(maxWidth: .infinity)
+                .padding(.vertical, 20)
+                .background(.regularMaterial)
+                .clipShape(Rectangle()).cornerRadius(20)
+                
+                Spacer()
+                Spacer()
+                
+                Text("Score: ???")
+                
+                    .foregroundStyle(.white)
+                    .font(.title.bold())
+                
+                
+                Spacer()
+                
             }
-            .alert(scoreTitel, isPresented: $showingScore) {
-                Button("Continue", action: askQuestion)
-            } message: {
-                Text("Your score is ???")
-            }
+            .padding()
+        }
+        .alert(scoreTitel, isPresented: $showingScore) {
+            Button("Continue", action: askQuestion)
+        } message: {
+            Text("Your score is ???")
         }
     }
     
